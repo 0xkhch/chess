e92bdd54f41afe3f450357443b45d73a5a41de82
Khaled <kh99ch@gmail.com>
2026-06-24 05:30:17 +0200

    small refactor

--- src/main.c
+++ src/main.c
@@ -6,6 +6,14 @@
 #include "move.h"
 #include "capture.h"
 
+typedef struct global_state {
+    int selected_x;
+    int selected_y;
+    e_piece selected_type;
+    e_state state;
+    bool turn;
+} global_state_t;
+
 //TODO: make these dynamic ?
 #define LIGHT_SHADE (Color){.r = 0xFF, .g = 0xCF, .b = 0x9F, .a = 0xFF}
 #define DARK_SHADE (Color){.r = 0xD2, .g = 0x8C, .b = 0x45, .a = 0xFF}
@@ -13,7 +21,6 @@
 #define t_WHITE false
 #define t_BLACK true
 
-bool turn = false;
 
 void draw_board(void)
 {
@@ -30,21 +37,23 @@ void draw_board(void)
         }
     }
 }
-void draw_pieces(Texture* pieces)
+void draw_pieces(Texture2D* pieces)
 {
-    Rectangle b_pawn = {0.0f, 0.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle b_knight = {128.0f, 0.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle b_bishop = {256.0f, 0.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle b_rook = {384.0f, 0.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle b_queen = {512.0f, 0.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle b_king = {640.0f, 0.0f, (float)pieces->width/6, (float)pieces->height/2};
+#define TEXTURE_WIDTH_COUNT (6)
+#define TEXTURE_HEIGHT_COUNT (2)
+    Rectangle b_pawn   = {0.0f, 0.0f,   (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle b_knight = {128.0f, 0.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle b_bishop = {256.0f, 0.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle b_rook   = {384.0f, 0.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle b_queen  = {512.0f, 0.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle b_king   = {640.0f, 0.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
 
-    Rectangle w_pawn = {0.0f,     128.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle w_knight = {128.0f, 128.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle w_bishop = {256.0f, 128.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle w_rook = {384.0f,   128.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle w_queen = {512.0f,  128.0f, (float)pieces->width/6, (float)pieces->height/2};
-    Rectangle w_king = {640.0f,   128.0f, (float)pieces->width/6, (float)pieces->height/2};
+    Rectangle w_pawn   = {0.0f,   128.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle w_knight = {128.0f, 128.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle w_bishop = {256.0f, 128.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle w_rook   = {384.0f, 128.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle w_queen  = {512.0f, 128.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
+    Rectangle w_king   = {640.0f, 128.0f, (float)pieces->width/TEXTURE_WIDTH_COUNT, (float)pieces->height/TEXTURE_HEIGHT_COUNT};
 
     Rectangle texture_dest = {.x = 0.0f, .y = 0.0f, .width = 64.0f, .height = 64.0f};
     for (int i = 0; i < GRID_Y; i++) {
@@ -105,6 +114,7 @@ void draw_pieces(Texture* pieces)
 
 int main(int argc, char** argv)
 {
+    global_state_t global = {};
     if (argc > 3) {
         fprintf(stderr, "%s\n", "Too many arguments...");
         return 1;
@@ -113,10 +123,10 @@ int main(int argc, char** argv)
         init_board(argv[argc - 2]);
         switch (*argv[argc - 1]) {
             case 'b':
-                turn = true;
+                global.turn = true;
             break;
             case 'w':
-                turn = false;
+                global.turn = false;
             break;
         }
     }
@@ -132,19 +142,11 @@ int main(int argc, char** argv)
     InitAudioDevice();
 
     Image pieces_img = LoadImageFromMemory(".png", pieces_png, pieces_png_len);
-    Texture pieces = LoadTextureFromImage(pieces_img);
-
-    Wave move_wav = LoadWaveFromMemory(".mp3", move_mp3, move_mp3_len);
+    Wave move_wav    = LoadWaveFromMemory(".mp3", move_mp3, move_mp3_len);
+    Wave capture_wav    = LoadWaveFromMemory(".mp3", capture_mp3, capture_mp3_len);
+    Texture2D pieces   = LoadTextureFromImage(pieces_img);
     Sound move_sound = LoadSoundFromWave(move_wav);
-
-    Wave capture_wav = LoadWaveFromMemory(".mp3", capture_mp3, capture_mp3_len);
     Sound capture_sound = LoadSoundFromWave(capture_wav);
-
-    e_state state = NONE;
-
-    int selected_x = 0;
-    int selected_y = 0;
-    e_piece selected_type = e_EMPTY;
 
     while (!WindowShouldClose()) { 
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
@@ -152,96 +154,95 @@ int main(int argc, char** argv)
             int x = (mouse_pos.x / CELL_X);
             int y = (mouse_pos.y / CELL_Y);
 
-            switch (state) {
+            switch (global.state) {
                 case NONE: {
 #ifdef DEBUG
+                    reset_possible_moves();
                     if (board[y * GRID_X + x] == 0) {
-                        state = NONE;
-                        selected_type = e_EMPTY;
-                        selected_x = 0;
-                        selected_y = 0;
-                        reset_possible_moves();
+                        global.state = NONE;
+                        global.selected_type = e_EMPTY;
+                        global.selected_x = 0;
+                        global.selected_y = 0;
                     }
                     else {
-                        state = SELECT;
-                        selected_x = x;
-                        selected_y = y;
-                        selected_type = board[y * GRID_X + x];
+                        global.state = SELECT;
+                        global.selected_x = x;
+                        global.selected_y = y;
+                        global.selected_type = board[y * GRID_X + x];
                     }
 #else
                     if ((board[y * GRID_X + x] == 0 
-                        || (board[y * GRID_X + x] < 0 && turn == t_WHITE) 
-                        || (board[y * GRID_X + x] > 0 && turn == t_BLACK))) {
-                        state = NONE;
-                        selected_type = e_EMPTY;
-                        selected_x = 0;
-                        selected_y = 0;
+                        || (board[y * GRID_X + x] < 0 && global.turn == t_WHITE) 
+                        || (board[y * GRID_X + x] > 0 && global.turn == t_BLACK))) {
+                        global.state = NONE;
+                        global.selected_type = e_EMPTY;
+                        global.selected_x = 0;
+                        global.selected_y = 0;
                         reset_possible_moves();
                     }
-                    else if ((board[y * GRID_X + x] > 0 && turn == t_WHITE) 
-                            || (board[y * GRID_X + x] < 0 && turn == t_BLACK)) {
-                        state = SELECT;
-                        selected_x = x;
-                        selected_y = y;
-                        selected_type = board[y * GRID_X + x];
+                    else if ((board[y * GRID_X + x] > 0 && global.turn == t_WHITE) 
+                            || (board[y * GRID_X + x] < 0 && global.turn == t_BLACK)) {
+                        global.state = SELECT;
+                        global.selected_x = x;
+                        global.selected_y = y;
+                        global.selected_type = board[y * GRID_X + x];
                     }
 #endif /* ifdef DEBUG */
                 } break;
                 case SELECT: {
 #ifdef DEBUG
                     if (possible_moves[y * GRID_X + x]) {
-                        state = NONE;
+                        global.state = NONE;
                         board[y * GRID_X + x] != e_EMPTY ? PlaySound(capture_sound) : PlaySound(move_sound);
-                        board[y * GRID_X + x] = board[selected_y * GRID_X + selected_x];
-                        board[selected_y * GRID_X + selected_x] = e_EMPTY;
-                        turn = !turn;
+                        board[y * GRID_X + x] = board[global.selected_y * GRID_X + global.selected_x];
+                        board[global.selected_y * GRID_X + global.selected_x] = e_EMPTY;
+                        global.turn = !global.turn;
 
-                        selected_type = e_EMPTY;
-                        selected_x = 0;
-                        selected_y = 0;
-                        reset_possible_moves();
+                        global.selected_type = e_EMPTY;
+                        global.selected_x = 0;
+                        global.selected_y = 0;
                     }
                     else {
-                        state = NONE;
+                        global.state = NONE;
                     }
+                    reset_possible_moves();
 #else
-                    if ((selected_y == y && selected_x == x) && ((board[y * GRID_X + x] > 0 && turn == t_WHITE) 
-                        || (board[y * GRID_X + x] < 0 && turn == t_BLACK))) {
-                        state = NONE;
-                        selected_type = e_EMPTY;
-                        selected_x = 0;
-                        selected_y = 0;
+                    if ((global.selected_y == y && global.selected_x == x) && ((board[y * GRID_X + x] > 0 && global.turn == t_WHITE) 
+                        || (board[y * GRID_X + x] < 0 && global.turn == t_BLACK))) {
+                        global.state = NONE;
+                        global.selected_type = e_EMPTY;
+                        global.selected_x = 0;
+                        global.selected_y = 0;
                         reset_possible_moves();
                         break;
                     }
-                    if ((board[y * GRID_X + x] > 0 && turn == t_WHITE) 
-                        || (board[y * GRID_X + x] < 0 && turn == t_BLACK)) {
-                        state = SELECT;
-                        selected_x = x;
-                        selected_y = y;
-                        selected_type = board[y * GRID_X + x];
+                    if ((board[y * GRID_X + x] > 0 && global.turn == t_WHITE) 
+                        || (board[y * GRID_X + x] < 0 && global.turn == t_BLACK)) {
+                        global.state = SELECT;
+                        global.selected_x = x;
+                        global.selected_y = y;
+                        global.selected_type = board[y * GRID_X + x];
                         reset_possible_moves();
                     }
-                    else if (!possible_moves[y * GRID_X + x] && ((board[y * GRID_X + x] < 0 && turn == t_WHITE) 
-                            || (board[y * GRID_X + x] > 0 && turn == t_BLACK))) {
-                        state = NONE;
-                        selected_type = e_EMPTY;
-                        selected_x = 0;
-                        selected_y = 0;
+                    else if (!possible_moves[y * GRID_X + x] && ((board[y * GRID_X + x] < 0 && global.turn == t_WHITE) 
+                            || (board[y * GRID_X + x] > 0 && global.turn == t_BLACK))) {
+                        global.state = NONE;
+                        global.selected_type = e_EMPTY;
+                        global.selected_x = 0;
+                        global.selected_y = 0;
                         reset_possible_moves();
                     }
                     else if ((possible_moves[y * GRID_X + x] 
-                            && ((board[y * GRID_X + x] <= 0 && turn == t_WHITE) 
-                            || (board[y * GRID_X + x] >= 0 && turn == t_BLACK)))) {
-                        state = NONE;
+                            && ((board[y * GRID_X + x] <= 0 && global.turn == t_WHITE) 
+                            || (board[y * GRID_X + x] >= 0 && global.turn == t_BLACK)))) {
+                        global.state = NONE;
                         board[y * GRID_X + x] != e_EMPTY ? PlaySound(capture_sound) : PlaySound(move_sound);
-                        board[y * GRID_X + x] = board[selected_y * GRID_X + selected_x];
-                        board[selected_y * GRID_X + selected_x] = e_EMPTY;
-                        turn = !turn;
-
-                        selected_type = e_EMPTY;
-                        selected_x = 0;
-                        selected_y = 0;
+                        board[y * GRID_X + x] = board[global.selected_y * GRID_X + global.selected_x];
+                        board[global.selected_y * GRID_X + global.selected_x] = e_EMPTY;
+                        global.turn = !global.turn;
+                        global.selected_type = e_EMPTY;
+                        global.selected_x = 0;
+                        global.selected_y = 0;
                         reset_possible_moves();
                     }
 #endif /* ifdef DEBUG */
@@ -252,15 +253,12 @@ int main(int argc, char** argv)
             ClearBackground(WHITE);
             draw_board();
             draw_pieces(&pieces);
-
-            switch (state) {
+            switch (global.state) {
                 case NONE: {
-
                 } break;
-
                 case SELECT: {
                     // TODO: compute this once mayhaps
-                    int num = find_possible_moves(selected_x, selected_y, selected_type);
+                    int num = find_possible_moves(global.selected_x, global.selected_y, global.selected_type);
                     for (size_t i = 0; i < GRID_Y; i++) {
                         for (size_t j = 0; j < GRID_X; j++) {
                             if (possible_moves[i * GRID_X + j]) {
@@ -269,9 +267,7 @@ int main(int argc, char** argv)
                         }
                     }
                 }; break;
-
                 default: {
-
                 } break;
             }
         EndDrawing();
